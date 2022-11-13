628e7f0db77db59a1bb48f87ee5b6a19c799309b
yaakov gottlieb <yaakov@webselenese.com>
2022-11-13 21:04:18 +0200

    write to file

---
+++ crawler.js
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import https from 'https';
+
+const results = [];
+
+const re = new RegExp('^(?:[a-z+]+:)?//', 'i');
+
+const fixUrl = (url, link) => {
+    if (!link) {
+        return '';
+    } else if (re.test(link)) {
+        return link;
+    } else if (link.startsWith('/') || link.startsWith('#')) {
+        return `${new URL(url).origin}${link}`;
+    } else {
+        return `${new URL(url).origin}/${link}`;
+    }
+}
+
+const crawl = async (url, depth) => {
+    try {
+        const agent = new https.Agent({
+            rejectUnauthorized: false
+        });
+        const response = await axios.get(url, { httpsAgent: agent });
+
+        if (response.status === 200) {
+
+            const $ = cheerio.load(response.data);
+
+            $('img').each((_, img) => {
+                results.push({
+                    imageUrl: fixUrl(url, img.attribs.src),
+                    sourceUrl: url,
+                    depth
+                })
+            });
+
+            if (depth < process.argv[3]) {
+                const links = $('a').toArray();
+                for (let link of links) {
+                    await crawl(fixUrl(url, link.attribs.href), depth + 1);
+                }
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+await crawl(process.argv[2], 0);
+
+fs.writeFile('results.json', JSON.stringify(results), (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Done!');
+    }
+});
